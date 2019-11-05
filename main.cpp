ba69066f07f676ac735ff3672cbe5ac0f8764a23
cnmullins <cnmullins@mail.csuchico.edu>
2019-11-04 16:06:35 -0800

    Completed lab 23.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
     bool acceptedLength(string);
     bool acceptedPhrase(string);
     void displayPassword(string);
+    int numberOfDigits(string);
 //---------------------------
 
 //-MAIN PROGRAM--------------
@@ -19,21 +20,17 @@ int main()
     string entry; // keeps track of what the user entered
     bool isGoodLength = false; // whether it is an acceptable length
     bool isGoodWord = false; // whether it is an acceptable word
+    int digits = 0;
  
     do
     {
-        // (2) call the getUserPassword function and store its return in entry
         entry = getUserPassword();
-        // (3) call the acceptedLength function w/ the entry argument
-        //     and store its return in isGoodLength
         isGoodLength = acceptedLength(entry);
-        // (4) call the acceptedPhrase function w/ the entry argument
-        //     and store its return in isGoodWord
         isGoodWord = acceptedPhrase(entry);
-    }while( !isGoodLength || !isGoodWord );
+        digits = numberOfDigits(entry);
+    }while( !isGoodLength || !isGoodWord || digits < 2 );
 
     cout<<"Password ";
-    // (5) call the displayPassword function w/ the entry argument
     displayPassword(entry);
     cout<<" has been set\n";
 
@@ -93,4 +90,21 @@ void displayPassword(string password)
         cout<<"*";
     }
 }
+
+int numberOfDigits (string input)
+{
+    int counter = 0;
+    for (int i = 0; i < input.size(); i++)
+    {
+        if (isdigit(input.at(i)))
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+
+
+
 //----------------------------
